8df7b9f97eaf9e1f6400e8bdcc7246b6e08e6d8d
Ambrose Chua <ambrose@chua.family>
2019-12-14 14:48:31 +0800

    Add API docs

--- func/add.js
+++ func/add.js
@@ -1,3 +1,12 @@
+/**
+ * @openapi
+ * /add:
+ *	 get:
+ *	   operationId: add
+ *	   description: Adds two numbers together
+ *	   x-pop-from-stack: 2
+ *	   x-push-to-stack: 1
+ */
 module.exports = (stack) => {
 	const b = stack.pop();
 	const a = stack.pop();
